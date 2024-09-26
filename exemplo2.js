60d9a47e00c8c9ac94a0de684b853d7ebf934a85
MateusIrineu <113316193+MateusIrineu@users.noreply.github.com>
2024-09-26 03:41:47 +0000

    exemplo2.js

--- exemplo2.js
+++ exemplo2.js
@@ -2,3 +2,12 @@
 
 const prompt = require("prompt-sync")()
 
+const NOTA = parseFloat(prompt("Insira sua nota: "))
+
+if (NOTA >= 7) {
+    console.log("O aluno está aprovado por média")
+} else if (NOTA < 3) {
+    console.log("O aluno está em reprovado")
+} else {
+    console.log("O aluno está em recuperação")
+}

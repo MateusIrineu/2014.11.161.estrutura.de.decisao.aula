505048e5a250b79a36b79a8b810ada70188b63b1
MateusIrineu <113316193+MateusIrineu@users.noreply.github.com>
2024-10-07 22:47:35 +0000

    exemplo4.js

--- exemplo4.js
+++ exemplo4.js
@@ -1,2 +1,15 @@
 
 
+const letra = 'a';
+
+switch(letra) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        console.log(`É VOGAL`);
+        break;
+    default:
+        console.log(`NÃO É VOGAL`);
+}
